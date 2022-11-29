452fd6ee91e16319c12927d2b0dbbedaa69ea190
gbonansea82 <111404910+gbonansea82@users.noreply.github.com>
2022-11-28 23:42:04 -0300

    add files

---
+++ ProyectoPresentacion/FrmActividadesRegistrar.Designer.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+namespace ProyectoPresentacion
+{
+    partial class FrmActividadesRegistrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.cmbProyectos = new System.Windows.Forms.ComboBox();
+            this.cmbActividades = new System.Windows.Forms.ComboBox();
+            this.cmbUsuarios = new System.Windows.Forms.ComboBox();
+            this.lblProyectos = new System.Windows.Forms.Label();
+            this.lblActividades = new System.Windows.Forms.Label();
+            this.lblUsuarios = new System.Windows.Forms.Label();
+            this.lblObservacion = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblHoraInicio = new System.Windows.Forms.Label();
+            this.lblHoraFin = new System.Windows.Forms.Label();
+            this.txtObservacion = new System.Windows.Forms.TextBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.dtpHoraInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtpHoraFin = new System.Windows.Forms.DateTimePicker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.Blue;
+            this.btnAceptar.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(392, 311);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 26);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // cmbProyectos
+            // 
+            this.cmbProyectos.FormattingEnabled = true;
+            this.cmbProyectos.Location = new System.Drawing.Point(84, 69);
+            this.cmbProyectos.Name = "cmbProyectos";
+            this.cmbProyectos.Size = new System.Drawing.Size(464, 21);
+            this.cmbProyectos.TabIndex = 1;
+            // 
+            // cmbActividades
+            // 
+            this.cmbActividades.FormattingEnabled = true;
+            this.cmbActividades.Location = new System.Drawing.Point(84, 109);
+            this.cmbActividades.Name = "cmbActividades";
+            this.cmbActividades.Size = new System.Drawing.Size(464, 21);
+            this.cmbActividades.TabIndex = 2;
+            // 
+            // cmbUsuarios
+            // 
+            this.cmbUsuarios.FormattingEnabled = true;
+            this.cmbUsuarios.Location = new System.Drawing.Point(84, 150);
+            this.cmbUsuarios.Name = "cmbUsuarios";
+            this.cmbUsuarios.Size = new System.Drawing.Size(464, 21);
+            this.cmbUsuarios.TabIndex = 3;
+            // 
+            // lblProyectos
+            // 
+            this.lblProyectos.AutoSize = true;
+            this.lblProyectos.BackColor = System.Drawing.Color.Red;
+            this.lblProyectos.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProyectos.Location = new System.Drawing.Point(20, 71);
+            this.lblProyectos.Name = "lblProyectos";
+            this.lblProyectos.Size = new System.Drawing.Size(58, 15);
+            this.lblProyectos.TabIndex = 4;
+            this.lblProyectos.Text = "Proyectos";
+            // 
+            // lblActividades
+            // 
+            this.lblActividades.AutoSize = true;
+            this.lblActividades.BackColor = System.Drawing.Color.Red;
+            this.lblActividades.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActividades.Location = new System.Drawing.Point(12, 111);
+            this.lblActividades.Name = "lblActividades";
+            this.lblActividades.Size = new System.Drawing.Size(66, 15);
+            this.lblActividades.TabIndex = 5;
+            this.lblActividades.Text = "Actividades";
+            // 
+            // lblUsuarios
+            // 
+            this.lblUsuarios.AutoSize = true;
+            this.lblUsuarios.BackColor = System.Drawing.Color.Red;
+            this.lblUsuarios.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarios.Location = new System.Drawing.Point(28, 152);
+            this.lblUsuarios.Name = "lblUsuarios";
+            this.lblUsuarios.Size = new System.Drawing.Size(50, 15);
+            this.lblUsuarios.TabIndex = 6;
+            this.lblUsuarios.Text = "Usuarios";
+            // 
+            // lblObservacion
+            // 
+            this.lblObservacion.AutoSize = true;
+            this.lblObservacion.BackColor = System.Drawing.Color.Red;
+            this.lblObservacion.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObservacion.Location = new System.Drawing.Point(7, 194);
+            this.lblObservacion.Name = "lblObservacion";
+            this.lblObservacion.Size = new System.Drawing.Size(71, 15);
+            this.lblObservacion.TabIndex = 7;
+            this.lblObservacion.Text = "Observacion";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.BackColor = System.Drawing.Color.Yellow;
+            this.lblFecha.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(39, 12);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(39, 15);
+            this.lblFecha.TabIndex = 8;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // lblHoraInicio
+            // 
+            this.lblHoraInicio.AutoSize = true;
+            this.lblHoraInicio.BackColor = System.Drawing.Color.Yellow;
+            this.lblHoraInicio.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraInicio.Location = new System.Drawing.Point(238, 13);
+            this.lblHoraInicio.Name = "lblHoraInicio";
+            this.lblHoraInicio.Size = new System.Drawing.Size(58, 15);
+            this.lblHoraInicio.TabIndex = 9;
+            this.lblHoraInicio.Text = "HoraInicio";
+            // 
+            // lblHoraFin
+            // 
+            this.lblHoraFin.AutoSize = true;
+            this.lblHoraFin.BackColor = System.Drawing.Color.Yellow;
+            this.lblHoraFin.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraFin.Location = new System.Drawing.Point(427, 13);
+            this.lblHoraFin.Name = "lblHoraFin";
+            this.lblHoraFin.Size = new System.Drawing.Size(49, 15);
+            this.lblHoraFin.TabIndex = 10;
+            this.lblHoraFin.Text = "HoraFin";
+            // 
+            // txtObservacion
+            // 
+            this.txtObservacion.Location = new System.Drawing.Point(84, 192);
+            this.txtObservacion.Multiline = true;
+            this.txtObservacion.Name = "txtObservacion";
+            this.txtObservacion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtObservacion.Size = new System.Drawing.Size(464, 102);
+            this.txtObservacion.TabIndex = 11;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(84, 12);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(99, 20);
+            this.dtpFecha.TabIndex = 12;
+            // 
+            // dtpHoraInicio
+            // 
+            this.dtpHoraInicio.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpHoraInicio.Location = new System.Drawing.Point(302, 13);
+            this.dtpHoraInicio.Name = "dtpHoraInicio";
+            this.dtpHoraInicio.Size = new System.Drawing.Size(66, 20);
+            this.dtpHoraInicio.TabIndex = 13;
+            // 
+            // dtpHoraFin
+            // 
+            this.dtpHoraFin.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpHoraFin.Location = new System.Drawing.Point(482, 13);
+            this.dtpHoraFin.Name = "dtpHoraFin";
+            this.dtpHoraFin.Size = new System.Drawing.Size(66, 20);
+            this.dtpHoraFin.TabIndex = 14;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Blue;
+            this.btnCancelar.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(473, 311);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 26);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // FrmActividadesRegistrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(560, 349);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.dtpHoraFin);
+            this.Controls.Add(this.dtpHoraInicio);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.txtObservacion);
+            this.Controls.Add(this.lblHoraFin);
+            this.Controls.Add(this.lblHoraInicio);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblObservacion);
+            this.Controls.Add(this.lblUsuarios);
+            this.Controls.Add(this.lblActividades);
+            this.Controls.Add(this.lblProyectos);
+            this.Controls.Add(this.cmbUsuarios);
+            this.Controls.Add(this.cmbActividades);
+            this.Controls.Add(this.cmbProyectos);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "FrmActividadesRegistrar";
+            this.Text = "FrmProyectoPresentacion";
+            this.Load += new System.EventHandler(this.FrmActividadesRegistrar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+     
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ComboBox cmbProyectos;
+        private System.Windows.Forms.ComboBox cmbActividades;
+        private System.Windows.Forms.ComboBox cmbUsuarios;
+        private System.Windows.Forms.Label lblProyectos;
+        private System.Windows.Forms.Label lblActividades;
+        private System.Windows.Forms.Label lblUsuarios;
+        private System.Windows.Forms.Label lblObservacion;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblHoraInicio;
+        private System.Windows.Forms.Label lblHoraFin;
+        private System.Windows.Forms.TextBox txtObservacion;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.DateTimePicker dtpHoraInicio;
+        private System.Windows.Forms.DateTimePicker dtpHoraFin;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ ProyectoPresentacion/FrmActividadesRegistrar.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Datos;
+using Negocio;
+
+namespace ProyectoPresentacion
+{
+    public partial class FrmActividadesRegistrar : Form
+    {
+        public FrmActividadesRegistrar()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmActividadesRegistrar_Load(object sender, EventArgs e)
+        {
+            this.cmbProyectos.SelectedIndexChanged -= cmbProyectos_SelectedIndexChanged;
+            LlenarComboProyectos();
+
+            this.cmbProyectos.SelectedIndexChanged += cmbProyectos_SelectedIndexChanged;
+
+            cmbProyectos.SelectedIndex = 1;
+            LlenarComboUsuarios();
+        }
+
+        void LlenarComboProyectos()
+        {
+            clsProyecto cls = new clsProyecto();
+            List<Proyectos> lista = cls.LeerProyectos();
+            cmbProyectos.DataSource = lista;
+            cmbProyectos.ValueMember = "Id";
+            cmbProyectos.DisplayMember = "Nombre";
+        }
+
+        void LlenarComboActividades()
+        {
+            clsActividades cls = new clsActividades();
+            int idProyectoSeleccionado = Convert.ToInt32(cmbProyectos.SelectedValue);
+            List<Actividades> lista = cls.LeerActividades(idProyectoSeleccionado);
+            cmbActividades.DataSource = lista;
+            cmbActividades.ValueMember = "Id";
+            cmbActividades.DisplayMember = "Nombre";
+        }
+
+        void LlenarComboUsuarios()
+        {
+            clsUsuario cls = new clsUsuario();
+            List<Usuarios> lista = cls.LeerUsuarios();
+            cmbUsuarios.DataSource = lista;
+            cmbUsuarios.ValueMember = "Id";
+            cmbUsuarios.DisplayMember = "Nombre";
+        }
+
+        private void cmbProyectos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LlenarComboActividades();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            TareasRealizadas entidad = new TareasRealizadas();
+            entidad.Id = 0;
+            entidad.Fecha = this.dtpFecha.Value;
+            entidad.HoraInicio = dtpHoraInicio.Value;
+            entidad.HoraFin = dtpHoraFin.Value;
+            entidad.IdActividad = Convert.ToInt32(cmbActividades.SelectedValue);
+            entidad.IdUsuario = Convert.ToInt32(cmbUsuarios.SelectedValue);
+            entidad.Comentario = txtObservacion.Text;
+
+            clsTareas cls = new clsTareas();
+            long idCreado = cls.Crear(entidad);
+            if (idCreado > 0)
+                MessageBox.Show("Tarea creada con éxito");
+            Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
